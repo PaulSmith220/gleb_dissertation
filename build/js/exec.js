62461a01cb2ac4cf3f7f549c1b30303bb8c1a561
p.kuznecov <kuznetps@gmail.com>
2016-05-01 11:24:05 +0500

    2 tmp_arrays

--- build/js/exec.js
+++ build/js/exec.js
@@ -1,20 +1,44 @@
 function execute() {
+    // Временный 3х-мерный массив, размер определяется длиной дискретного времени
+    var tmp_R3_height = parseFloat($("#discretTimeLength").val());
+    var tmp_R3 = new Array(tmp_R3_height);
+    // В первый слой копируем тензор "Начальное значение тензора импульсного воздействия", остальное заполняем нулями
+    var k = parseInt($("#graphVerticesAmount").val()),
+        j =  parseInt($("#systemCount").val());
 
-
-    //Обновляем входа
-    for(var i = 0; i < dataSet.input.length; i++) {
-        var $elem = document.getElementById(dataSet.input[i].id).value*1;
-        dataSet.input[i].value =  $elem;
-        console.log(dataSet.input[i].value);
+    tmp_R3[0] = [];
+    for (var _k = 0; _k < k; _k++) {
+        tmp_R3[0][_k] = [];
+        for (var _j = 0; _j < j; _j++) {
+            tmp_R3[0][_k][_j] = dataSet.w_tenzor_vectorR2[_j][_k];
+        }
     }
-
-
-    // Вывод отдельного состояния
-    for (var i = 0; i < dataSet.output.length; i++) {
-        var v = dataSet.output[i].formula(0);
-        dataSet.output[i].value = v;
-        document.getElementById(dataSet.output[i].id).innerHTML = v;
+    for (var layer = 1; layer < tmp_R3_height; layer++) {
+        tmp_R3[layer] = [];
+        for (var _k = 0; _k < k; _k++) {
+            tmp_R3[layer][_k] = [];
+            for (var _j = 0; _j < j; _j++) {
+                tmp_R3[layer][_k][_j] = 0;
+            }
+        }
     }
+    console.log("Временный 3х-мерный вектор", JSON.stringify(tmp_R3));
+
+    // Временный 2х-мерный массив, размер определяется длиной дискретного времени
+    var tmp_R2_height = parseFloat($("#discretTimeLength").val());
+    var tmp_R2 = new Array(tmp_R2_height);
+    tmp_R2[0] = [];
+    for (var i = 0; i < dataSet.w_tenzor_vector.length; i++) {
+        tmp_R2[0][i] = dataSet.w_tenzor_vector[i];
+    }
+    for (var layer = 1; layer < tmp_R2_height; layer++) {
+        tmp_R2[layer] = [];
+        for (var i = 0; i < dataSet.w_tenzor_vector.length; i++) {
+            tmp_R2[layer][i] = 0;
+        }
+    }
+    console.log("Временный 2х-мерный вектор", JSON.stringify(tmp_R2));
+
 
 
 }
